6eacc7013e132d5084d2ce04ee24450a4a43e12d
Janne Spijkervet <janne.spijkervet@gmail.com>
2021-01-23 22:56:48 +0100

    merge

--- web/latent_listening/get_predictions.py
+++ web/latent_listening/get_predictions.py
@@ -3,6 +3,7 @@ import torch
 import torchaudio
 import numpy as np
 from collections import defaultdict
+from tqdm import tqdm
 from utils import parse_args
 from data import get_dataset
 from model import load_encoder
@@ -11,7 +12,7 @@ if __name__ == "__main__":
     args = parse_args("./config/config.yaml", addit=[])
     args.world_size = 1
     args.supervised = False
-    args.dataset = "magnatagatune"
+    args.dataset = "msd"
     args.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     args.model_path = "./logs/66"
@@ -34,6 +35,8 @@ if __name__ == "__main__":
     encoder.eval()
     encoder = encoder.to(args.device)
 
+    print("LOADED")
+
     model = None
     if not args.supervised:
         finetuned_head = torch.nn.Sequential(
@@ -49,6 +52,7 @@ if __name__ == "__main__":
             )
         )
         finetuned_head = finetuned_head.to(args.device)
+        print("LOADED FINETUNED HEAD")
 
     # initialize TensorBoard
 
@@ -59,33 +63,42 @@ if __name__ == "__main__":
     predictions = []
     n = 10 # add every n
     with torch.no_grad():
-        for step, (track_id, clip_id, segment, fp, label) in enumerate(test_dataset.index):
+        for step, (track_id, clip_id, segment, fp, label) in enumerate(tqdm(test_dataset.index)):
             if step % n == 0:
                 audio = test_dataset.get_full_size_audio(fp)
                 audio = audio.to(args.device)
                 h = encoder(audio)
+                h = torch.nn.functional.softmax(h, dim=1)
                 output = finetuned_head(h)
                 output = torch.nn.functional.softmax(output, dim=1)
 
                 h = h.mean(dim=0)
                 output = output.mean(dim=0) # take mean predictions of whole track, i.e., over batch dim.
                 predictions.append([output.cpu().numpy().tolist(), h.cpu().numpy().tolist(), track_id, clip_id, segment, fp, label])
-
     #             print(step, "/", len(test_dataset))        
 
+    outputs = np.array([o[0] for o in predictions])
+    hs = np.array([o[1] for o in predictions])
+
+    # print(outputs.shape, hs.shape)
+    # pearson = np.corrcoef(outputs, hs)
+    # print(pearson.shape)
+    # exit()
+
     # for website
+    print("Preparing .json for web listening interface")
     ds = []
-    for idx, a in enumerate(predictions):
+    for idx, a in enumerate(tqdm(predictions)):
         preds_finetuned = a[0]
         preds_encoder = a[1]
 
         d = {}
 
         # for faster loading in web browser
-        mp3_fp = os.path.splitext(a[5])[0] + ".wav"
-    #     if not os.path.exists(mp3_fp):
-    #         audio, sr = torchaudio.load(a[5])
-    #         torchaudio.save(mp3_fp, audio, sr)
+        mp3_fp = os.path.splitext(a[5])[0] + ".mp3" # ".wav"
+        if not os.path.exists(mp3_fp):
+            audio, sr = torchaudio.load(a[5])
+            torchaudio.save(mp3_fp, audio, sr)
 
         d["idx"] = idx
         d["audio"] = mp3_fp
@@ -107,5 +120,5 @@ if __name__ == "__main__":
     #     print(idx, "/", len(predictions))
 
     import json
-    with open("./web/latent_listening/predictions.json", "w") as f:
+    with open("./web/latent_listening/predictions_{}.json".format(args.dataset), "w") as f:
         json.dump(ds, f)
